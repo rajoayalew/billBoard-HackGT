02ecd5d45af4cfd592f19d8c85d0b7f8d861b84e
Raey Ayalew <raeyayalew@gmail.com>
2022-10-22 05:49:46 -0400

    Added some simply documentation

--- testcases/testdata.py
+++ testcases/testdata.py
@@ -1,6 +1,8 @@
 import csv
 import random
 
+# Generates a .csv with data to be used for website test cases
+
 states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN',
 'IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ',
 'NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA',
