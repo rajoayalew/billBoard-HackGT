f7b280fcacd8becaddc05e77681a83ecde503b77
samyukta-iyer <samyuktasainath@gmail.com>
2022-10-22 19:19:33 -0400

    g

--- Front/template/assets/js/script.js
+++ Front/template/assets/js/script.js
@@ -1,13 +1,13 @@
-var gener8Estim = document.getElementById('generateEstim');
-gener8Estim.onclick(open, 'answer.html');
+// var gener8Estim = document.getElementById('generateEstim');
+// gener8Estim.onclick(open, 'answer.html');
 
 
-alert("this works");
+// alert("this works");
 
-var buttontest = document.getElementById("test");
-buttontest.onclick(open, 'post.html');
+// var buttontest = document.getElementById("test");
+// buttontest.onclick(open, 'post.html');
 
 
-var proc = document.getElementById('#procedures');
-var selectedProc = proc.ariaValueMax;
+// var proc = document.getElementById('#procedures');
+// var selectedProc = proc.ariaValueMax;
 
