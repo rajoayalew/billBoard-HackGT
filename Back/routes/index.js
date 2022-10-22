39947be1a256da3159023f7dd0c785f1b48c09e3
Raey Ayalew <raeyayalew@gmail.com>
2022-10-22 19:04:16 -0400

    Add index file

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -13,12 +13,10 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req.body)
 
-  
   let query = "SELECT * FROM entries";
   db.all(query, function (err, rows) {
     console.log("FIRST ROW:", rows[0]);
   });
-
 
   console.log(db);
   res.send({msg: "hi"})
