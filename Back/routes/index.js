0dd92b684e06dbfe54ce00347af381357afba39e
Raey Ayalew <raeyayalew@gmail.com>
2022-10-22 23:00:50 -0400

    Moved data.db file which allowed made the append thingy work

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   console.log(req)
-//   res.send({msg: "hi"})
-// });
-
 router.post('/', function(req, res, next) {
   var data = req.body;
   var Procedure = Number(data.Procedure);
@@ -16,41 +9,15 @@ router.post('/', function(req, res, next) {
   var Gender = data.Gender;
   var Age = Number(data.Age);
 
-  const sqlite3 = require('sqlite3').verbose();
-  let db = new sqlite3.Database('data.db');
+  var output = [Procedure, Cost, State, Gender, Age]
 
-  db.run('INSERT INTO entries VALUES (NULL, "Procedure", datetime("now", "localtime") ,"Cost","State", ,"Gender", "Age")');
+  const sqlite3 = require('sqlite3').verbose();
+  let db = new sqlite3.Database("data.db");
+
+  db.run('INSERT INTO entries VALUES (NULL, ?, datetime("now", "localtime"), ?, ?, ?, ?)', output);
 
   db.close();
   res.send({msg: "bruh"});
-
-  // output = [Procedure, Cost, Age, Gender, State];
-
-
-  // let query = "INSERT INTO entries VALUES(NULL," +
-  //             Procedure + "datetime('now', 'localtime')," +
-  //             Cost + "," + Age + "," + Gender + "," + State + ")"
-  //let query = "INSERT INTO entries VALUES(NULL, ?, datetime('now', 'localtime'), ?, ?, ?, ?)"
-  // db.all(query, [output], (err, rows) => {
-  //     console.log("FIRST ROW:", rows[0]);
-  //   });
-
-  // db.serialize(function() {
-  //   var stmt = db.prepare("INSERT INTO entries VALUES(NULL, ?, datetime('now', 'localtime'), ?, ?, ?, ?)");
-  //   stmt.run(Procedure, Cost, State, Gender, Age);
-  //   stmt.finalize();
-  
-  //   db.each("SELECT * FROM entries", function(err, row) {
-  //       console.log(row[1]);
-  //   });
-  // });
-  
-
-  // db.run(query, [output],function(err) {
-  //   if (err) {
-  //     return console.log(err.message);
-  //   }
-  // });
   
 });
 
