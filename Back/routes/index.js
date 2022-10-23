f79ad8ea158c95fafb2558a7af22376f7c845f97
Rikhil Seshadri <rikhilseshadri@gmail.com>
2022-10-22 21:18:41 -0400

    get data from form

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('data.db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req)
-  res.send({msg: "hi"})
-});
+// router.get('/', function(req, res, next) {
+//   console.log(req)
+//   res.send({msg: "hi"})
+// });
 
 router.post('/', function(req, res, next) {
   var data = req.body;
@@ -18,7 +16,15 @@ router.post('/', function(req, res, next) {
   var Gender = data.Gender;
   var Age = Number(data.Age);
 
-  output = [Procedure, Cost, Age, Gender, State];
+  const sqlite3 = require('sqlite3').verbose();
+  let db = new sqlite3.Database('data.db');
+
+  db.run('INSERT INTO entries VALUES (NULL, "Procedure", datetime("now", "localtime") ,"Cost","State", ,"Gender", "Age")');
+
+  db.close();
+  res.send({msg: "bruh"});
+
+  // output = [Procedure, Cost, Age, Gender, State];
 
 
   // let query = "INSERT INTO entries VALUES(NULL," +
@@ -29,15 +35,15 @@ router.post('/', function(req, res, next) {
   //     console.log("FIRST ROW:", rows[0]);
   //   });
 
-  db.serialize(function() {
-    var stmt = db.prepare("INSERT INTO entries VALUES(NULL, ?, datetime('now', 'localtime'), ?, ?, ?, ?)");
-    stmt.run(Procedure, Cost, State, Gender, Age);
-    stmt.finalize();
+  // db.serialize(function() {
+  //   var stmt = db.prepare("INSERT INTO entries VALUES(NULL, ?, datetime('now', 'localtime'), ?, ?, ?, ?)");
+  //   stmt.run(Procedure, Cost, State, Gender, Age);
+  //   stmt.finalize();
   
-    db.each("SELECT * FROM entries", function(err, row) {
-        console.log(row[1]);
-    });
-  });
+  //   db.each("SELECT * FROM entries", function(err, row) {
+  //       console.log(row[1]);
+  //   });
+  // });
   
 
   // db.run(query, [output],function(err) {
@@ -45,10 +51,9 @@ router.post('/', function(req, res, next) {
   //     return console.log(err.message);
   //   }
   // });
-
-  res.send({msg: "hi"})
+  
 });
 
-db.close();
+
 
 module.exports = router;
